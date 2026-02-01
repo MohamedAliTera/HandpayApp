e868bcd155651236b79c6b4f3f4705ae1debad0a
MohamedA-Ibrahim <mohamed.ib1751@gmail.com>
2026-02-01 13:14:09 +0200

    Add project files.

---
+++ HandpayApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HandpayApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ HandpayApp/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HandpayApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
